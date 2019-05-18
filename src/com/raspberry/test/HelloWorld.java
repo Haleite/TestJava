a95d0b53d7fcf5e9d7d78942513101721aae4e15
lanthanum <768310524@qq.com>
2019-05-18 13:14:04 +0800

    create TestJava

---
+++ src/com/raspberry/test/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.raspberry.test;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Date d = new Date();
+//        List l = new ArrayList();
+        String[] arr ={"A", "B", "C", "D"} ;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public void test1(){
+        new ArrayList()
+    }
+}
